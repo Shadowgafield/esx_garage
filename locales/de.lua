b2c9e50f9b0e6f6579a4562dd212793df7e7d05a
Shadowgafield <55643642+Shadowgafield@users.noreply.github.com>
2019-09-22 19:09:23 +0200

    Update de.lua

--- locales/de.lua
+++ locales/de.lua
@@ -1,5 +1,5 @@
 Locales['de'] = {
 	['veh_released'] = 'Fahrzeug ~g~genommen',
 	['veh_stored'] = 'vehicle ~g~geparkt',
-	['veh_health'] = 'you\'ll have to repair the vehicle before storing it.',
+	['veh_health'] = 'Sie müssen das Fahrzeug reparieren, bevor Sie es lagern.',
 }
